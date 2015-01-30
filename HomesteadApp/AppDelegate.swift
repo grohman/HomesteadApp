4c0c3b82e9bb46619fb9e14c372299c8ee751449
Daniel Podrabinek <danyagrohman@gmail.com>
2015-01-30 23:33:32 +0300

    Initial Commit

---
+++ HomesteadApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HomesteadApp
+//
+//  Created by Grohman on 30.01.15.
+//  Copyright (c) 2015 Grohman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
